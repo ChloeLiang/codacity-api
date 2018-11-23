65f113d2f71e6fda51ecbe6307cd89b01309e88c
ChloeLiang <nliangxin@gmail.com>
2018-11-23 14:37:08 +0800

    Enable CORS

--- server/server.js
+++ server/server.js
@@ -13,6 +13,15 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app = require('./routes')(app);
 
 app.listen(port, () => {
